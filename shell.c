6b39c32ead02cf507edadf9f3b586c48ef2bd4d5
rahavan-siva <rahavan2003@outlook.com>
2024-07-03 19:28:00 -0600

    Adding functions

--- shell.c
+++ shell.c
@@ -4,8 +4,10 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <sys/utsname.h>
 #include <readline/readline.h>
 #include <readline/history.h>
+#include <dirent.h>
 #include "shell.h"
 
 void clearScreen(){
@@ -13,17 +15,93 @@ void clearScreen(){
 }
 
 void startProgram(){
+    struct utsname systemInfo;
     clearScreen();
-    char *username =(char*)(malloc(256*sizeof(char)));
-    char *hostname = (char*)(malloc(256*sizeof(char)));
+    char *username =(char*)(malloc(1024*sizeof(char)));
     username = getenv("USER");
     printf("Welcome to the Shell!\n");
-    if(gethostname(hostname, sizeof(hostname)) != 0){
-        fprintf(stderr, "Error: cannot get host name!");
+    if(uname(&systemInfo)!= 0){
+        fprintf(stderr, "Error: cannot get system information!");
     }
-    printf("%s@%s: ", username, hostname);
+    printf("%s@%s: ", username, systemInfo.nodename);
 }
 
+int readCommandLine(char* input){
+    char* inputBuffer;
+    inputBuffer = readline("% ");
+    if(strlen(inputBuffer) != 0){
+        add_history(inputBuffer);
+        strcpy(input, inputBuffer);
+        return 0;
+    }
+    else{
+        free(inputBuffer);
+        return 1;
+    }
+}
+
+void parseInput(char *input, char** parsedInput){
+    if(strlen(input) == 0){
+        printf("Command not found");
+    }
+    for(int i = 0; i < strlen(input); i++){
+        parsedInput[i] = strsep(&input, " ");
+        if(parsedInput[i] == NULL){
+            break;
+        }
+    }
+}
+
+void printDirectoryPath(){
+    char cwd[1024];
+    if(getcwd(cwd, sizeof(cwd)) == NULL){
+        fprintf(stderr, "Cannot retrieve currrent directory path!");
+    }
+    printf("%s\n", cwd);
+}
+
+void printDirectoryContents(){
+    struct dirent *content;
+    char cwd[1024];
+    if(getcwd(cwd, sizeof(cwd)) == NULL){
+        fprintf(stderr, "Cannot retrieve currrent directory path!");
+    }
+    DIR *directory = opendir(cwd);
+    if(directory == NULL){
+        fprintf(stderr, "Cannot open current directory path");
+    }
+    while((content = readdir(directory))){
+        if((strcmp(content->d_name, ".") == 0) || (strcmp(content->d_name, "..") == 0)){
+            continue;
+        }
+        else{
+            printf("%s\n",content->d_name);
+        }
+    }
+    closedir(directory);
+}
+
+void changeDirectories(char *input){
+    char workBuf[1024];
+    parseInput(input, workBuf);
+    printf("%s\n",workBuf);
+    //chdir(&(workBuf[1]));
+}
+
+void commandSupport(){
+    printf("\nCommands supported by my Shell:"
+    "\nls"
+    "\ncd"
+    "\npwd"
+    "\nexit"
+    "\ncode compilation"
+    "\n"
+
+    
+    
+    
+    );
+}
 
 
 
--- shell.h
+++ shell.h
@@ -7,12 +7,18 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <sys/utsname.h>
 #include <readline/readline.h>
 #include <readline/history.h>
+#include <dirent.h>
 
 void startProgram();
 void clearScreen();
+void parseInput(char* input, char** parsedInput);
+int readCommandLine(char* input);
 void printDirectoryPath();
 void printDirectoryContents();
+void changeDirectories(char *input);
+
 
 #endif
